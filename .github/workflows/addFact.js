bb318947b3d621c0513cf05108f6284a7e7f4384
Greg Walker (he/him) <mgwalker@users.noreply.github.com>
2021-09-14 15:03:58 -0500

    Automatically create facts from properly-formatted issues (#114)
    * first pass
    
    * okay, let's try it... probably won't work but what the hey
    
    * helps if the yaml is valid

---
+++ .github/workflows/addFact.js
@@ -0,0 +1,95 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { exec } = require("child_process");
+const { Octokit } = require("@octokit/rest");
+
+const exists = async (path) => {
+  try {
+    await fs.access(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const getFact = (rawFact) => {
+  if (/^{[\s\S]+}$/m.test(rawFact)) {
+    try {
+      return JSON.parse(rawFact);
+    } catch (e) {
+      console.log("There was a problem parsing this fact.");
+      console.log(e);
+      throw e;
+    }
+  }
+  return rawFact;
+};
+
+const cmd = (...cmd) =>
+  new Promise((resolve, reject) => {
+    exec(...cmd, (err, stdout, stderr) => {
+      if (err) {
+        reject(new Error(err));
+      } else {
+        resolve({ stdout, stderr });
+      }
+    });
+  });
+
+(async () => {
+  const github = new Octokit({
+    token: process.env.GITHUB_TOKEN,
+    userAgent: "18f bot facts",
+  });
+
+  const {
+    issue: { number: issueNumber },
+    repository: {
+      name: repo,
+      owner: { login: owner },
+    },
+  } = JSON.parse(
+    await fs.readFile(process.env.GITHUB_EVENT_PATH, { encoding: "utf-8" })
+  );
+
+  const {
+    data: { body },
+  } = await github.rest.issues.get({ owner, repo, issue_number: issueNumber });
+
+  const [, type, rawFact] =
+    body.match(
+      /^### What kind of fact are you adding\?\n\n([^\n]+)\n\n### Your new fact:\n\n(.*)$/im
+    ) ?? [];
+
+  const allFactsPath = path.join(__dirname, "..", "..", `${type}.json`);
+
+  if (!type.length || !rawFact.length) {
+    return;
+  }
+  if (!(await exists(allFactsPath))) {
+    return;
+  }
+
+  const fact = getFact(rawFact);
+  const allFacts = JSON.parse(
+    await fs.readFile(allFactsPath, { encoding: "utf-8" })
+  );
+  allFacts.push(fact);
+  await fs.writeFile(allFactsPath, JSON.stringify(allFacts, null, 2), {
+    encoding: "utf-8",
+  });
+
+  const branch = `add-fact/${issueNumber}`;
+  await cmd(`git checkout -b ${branch}`);
+  await cmd(`git add ${allFactsPath}`);
+  await cmd(`git push origin ${branch}`);
+
+  await github.pulls.create({
+    base: "main",
+    body: `This fact is being added automatically from the contents of #${issueNumber}`,
+    head: branch,
+    owner,
+    repo,
+    title: `Adding fact for #${issueNumber}`,
+  });
+})();
